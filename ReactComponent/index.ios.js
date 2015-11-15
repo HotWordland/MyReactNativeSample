7a0f97feb0c2c75169181afae638a40e247ce706
巫龙 <454763196@qq.com>
2015-11-15 15:24:43 +0800

    # started githubFinderSample

--- ReactComponent/index.ios.js
+++ ReactComponent/index.ios.js
@@ -5,13 +5,39 @@ var React = require('react-native');
 var BASE_URL = 'https://api.github.com/search/repositories?q=';
 
 var {
+  AppRegistry,
   StyleSheet,
   View,
   TextInput,
+  Text,
+  ListView,
+  Image,
 } = React;
 
 var MyReactNativeSample = React.createClass({
+  getInitialState : function() {
+  return {
+    dataSource : new ListView.DataSource({
+      rowHasChanged:(row1,row2) => row1 !== row2,
+    }),
+  };
+  },
   render: function() {
+    var content;
+      if (this.state.dataSource.getRowCount() === 0) {
+        content = <Text>Please enter a search term to see results.</Text>
+      }else{
+        content = 
+        <ListView
+        ref="ListView"
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
+        automaticallyAdjustContentInsets={false}
+        keyboradDismissMode="OnDrag"
+        keyboradShouldPersistTags={true}
+        showsVerticalScrollIndicator={true} 
+        />;
+      }
     return (
       <View style={styles.container}>
       <TextInput
@@ -21,11 +47,33 @@ var MyReactNativeSample = React.createClass({
                style={styles.searchBarInput}
                onEndEditing={this.onSearchChange}
       />
+      {content}
       </View>
     );
   },
+  renderRow : function(repo : Object) {
+  return(
+    <View>
+      <View style={styles.row}>
+        <Image
+          style={styles.profpic}
+          source={{uri: repo.owner.avatar_url}} />
+        
+        <View style={styles.textContainer}>
+        <Text>{repo.name}</Text>
+     
+
+        <Text>地址:{repo.archive_url}</Text>
+
+        <Text>描述:{repo.description}</Text>
+       </View>
+        </View>
+        <View style={styles.cellBorder} />  
+    </View>
+    );
+  },
   onSearchChange : function(event : Object){
-   var searchTerm = event.nativeEvent.text.tolowerCase();
+   var searchTerm = event.nativeEvent.text.toLowerCase();
    var queryURL = BASE_URL + encodeURIComponent(searchTerm);
    fetch(queryURL)
    .then((response) => response.json())
@@ -35,12 +83,9 @@ var MyReactNativeSample = React.createClass({
         dataSource : this.state.dataSource.cloneWithRows
         (responseData.items),
       });
-      }
     }
-   })
-   .done();
-
-  }
+   }).done();
+ },
 });
 
 
@@ -56,7 +101,26 @@ var styles = StyleSheet.create({
     //flex : 1,
     height : 30,
     backgroundColor : '#EAEAEA',
-  }
+  },
+  row:{
+    alignItems : 'flex-start',
+    backgroundColor:'white',
+    // flexDirection:'column',
+    padding:5,
+  },
+  cellBorder:{
+    backgroundColor:'rgb(0,0,0,1)',
+    height:1,
+    marginLeft:4,
+  },
+  profpic:{
+     width:50,
+     height:50,
+  },
+  textContainer:{
+    marginLeft:60,
+    marginTop:-50,
+  },
 });
 
 
