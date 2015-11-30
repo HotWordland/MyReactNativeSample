cdb5036fa7790dc20f6dc2ced9486b0f99413909
WL <454763196@qq.com>
2015-11-30 22:46:32 +0800

    # init AnimationScene

--- ReactComponent/index.ios.js
+++ ReactComponent/index.ios.js
@@ -2,49 +2,48 @@
 'use strict';
 
 var React = require('react-native');
-var BASE_URL = 'https://api.github.com/search/repositories?q=';
-
-
-
 var {
   AppRegistry,
   StyleSheet,
   View,
-  TextInput,
   Text,
-  ListView,
-  Image,
   NavigatorIOS,
   TouchableHighlight,
+  Image,
 } = React;
 
 var MyReactNativeSample = React.createClass({
+  //在这里声明了一个变量(在React-Native里面叫做状态) 有什么用呢？在stackoverflow里找到一句话 "React will automatically do a re-render when state or props change, so there's no need for this." 就是说当我们改变
+  //了状态的值 那么 React就会自动地重新渲染界面 执行render方法
    getInitialState: function() {
     return {
-      openExternalExample: (null: ?false),
+      openNavigatorExample: (null: ?false),
     };
   },
+  //渲染
   render: function() {
-    if (this.state.openExternalExample) {
+    //判断一下是不是打开了navigator的例子是的话我们就将界面重新渲染成navigatorExample 不是的话那么就渲染成本来的样子
+    if (this.state.openNavigatorExample) {
        var NavigatorExample = require('./Navigator/Navigator')
       return (
         <NavigatorExample
-        onExit={() => {
-            this.setState({ openExternalExample: null, });
+          onExit={() => {
+            this.setState({ openNavigatorExample: null, });
           }}
         />
       );
     }
 
     return(
+      //React-Native的NavigatorIOS控件 详细的用法和信息可以在官方文档搜索
       <NavigatorIOS
         style={styles.container}
         initialRoute={{
           title: 'Home',
           component: HomeIndexScene,
           passProps: {
-              onExternalExampleRequested: (mark) => {
-              this.setState({ openExternalExample: mark, });
+              onNavigatorExampleRequested: (mark) => {
+              this.setState({ openNavigatorExample: mark, });
             },
             },
         }}
@@ -56,82 +55,99 @@ var MyReactNativeSample = React.createClass({
     )
   }
 });
+class NaviBubble extends React.Component {
+  render(){
+    return(
+      <TouchableHighlight onPress={this.props.ClickAction}
+                  >   
+      <View style={styles.messageBox}>
+        <View>
+            <Text style={styles.messageBoxTitleText}>{this.props.Title}</Text>
+        </View>
+        <View>
+            <Text style={styles.messageBoxBodyText}>{this.props.Descri}</Text>
+        </View>
+    </View>
+    </TouchableHighlight>
+      );
+  }
+}
+//NavigatorIOS 第一个Scene
 var HomeIndexScene = React.createClass({
   render : function(){
     return(
     
                  <View style = {styles.content}> 
-                  <TouchableHighlight onPress={() => {
-                    var GitFinderScene  = require('./GitFinder/GitFinder');
-
+                  
+                  <NaviBubble
+                    Title="GitFinder"
+                    Descri="搜索github的仓库"
+                    navigator={this.props.navigator}
+                    ClickAction={()=>{
+                       var GitFinderScene  = require('./GitFinder/GitFinder');
+                    //这里使用的NavigatorIOS进行跳转
                     this.props.navigator.push({
                       title: "GitFinder",
                       component: GitFinderScene,
                     });
-                  }
-                  }>   
-                    <View style={styles.messageBox}>
-                        <View>
-                            <Text style={styles.messageBoxTitleText}>GitFinder</Text>
-                        </View>
-                        <View>
-                            <Text style={styles.messageBoxBodyText}>搜索github的仓库.</Text>
-                        </View>
-                    </View>
-                   </TouchableHighlight>
 
-                  <TouchableHighlight onPress={() => {
-                    // var NavigatorScene  = require('./Navigator/Navigator');
+                    }}
+                    />
+                   
 
-                    // this.props.navigator.push({
-                    //   title: "NavigatorExample",
-                    //   component: NavigatorScene,
-                    // });
-                    this.props.onExternalExampleRequested(true);
+                  
 
-                  }
-                  }>   
-                   <View style={styles.messageBox}>
-                        <View>
-                            <Text style={styles.messageBoxTitleText}>Navigator</Text>
-                        </View>
-                        <View>
-                            <Text style={styles.messageBoxBodyText}>Facebook-js实现Navi方案</Text>
-                        </View>
-                    </View>
-                    </TouchableHighlight>
+              
+                    <NaviBubble
+                    Title="Navigator"
+                    Descri="Facebook-js实现Navi方案"
+                    navigator={this.props.navigator}
+                    ClickAction={()=>{
+                    this.props.onNavigatorExampleRequested(true);
+                    }
+                    }
+                    />
+              
 
-                    <View style={styles.messageBox}>
-                        <View>
-                            <Text style={styles.messageBoxTitleText}>更新中...</Text>
-                        </View>
-                        <View>
-                            <Text style={styles.messageBoxBodyText}>更新中...</Text>
-                        </View>
-                    </View>
+                    <NaviBubble
+                    Title="动画"
+                    Descri="React-Native动画"
+                    navigator={this.props.navigator}
+                    ClickAction={()=>{
+                      var AnimationScene = require('./Animation/Animation')
+                      this.props.navigator.push({
+                      title: "Animation",
+                      component: AnimationScene,
+                    });
+                    }}
+                    />
+
+                    <NaviBubble
+                    Title="更新中..."
+                    Descri="更新中"
+                    navigator={this.props.navigator}
+                    />
+
+                    <Image source={require('image!uie_thumb_big')} style={styles.icon} />
 
                   </View>
       )
   }
 })
 
+//样式
 var styles = StyleSheet.create({
   container:{
    flex : 1,
-   //backgroundColor : "black",
-
-
   },
   content : {
-  backgroundColor:'#ebeef0',
-        // flex:1,
    marginTop : 64,
    marginLeft : 0,
    marginRight : 0,
    marginBottom : 0,
    flexDirection:'column',
-        alignItems:'center',
-        //justifyContent:'center'
+   alignItems:'center',
+  //justifyContent:'center'
   },
    messageBox:{
         marginTop : 5,
@@ -142,8 +158,6 @@ var styles = StyleSheet.create({
         paddingLeft:20,
         paddingRight:20, 
         borderRadius:10,
-
-
     },
     messageBoxTitleText:{
         fontWeight:'bold',
@@ -157,7 +171,11 @@ var styles = StyleSheet.create({
         textAlign:'center',
         fontSize:16
     },
+    icon: {
+    width: 30,
+    height: 30,
+  },
 });
 
-
+//注册程序的入口
 React.AppRegistry.registerComponent('MyReactNativeSample', () => MyReactNativeSample);
